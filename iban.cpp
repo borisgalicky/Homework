0e0047cce1fb58fb31ecb4b7360a0df1e9f7d16a
borisgalicky <32737469+borisgalicky@users.noreply.github.com>
2017-11-13 22:55:28 +0100

    updated iban.cpp and portfolio html
    Added updated version of iban.cpp containing loops for inserting numbers to strings (not working) and portfolio (index.html)

--- iban.cpp
+++ iban.cpp
@@ -1,3 +1,4 @@
+
 /*
 Author: Boris Galický, 1.N
 Date: 10-11-2017
@@ -12,6 +13,7 @@ int main()
 {
 	int bankcode;
 	long long number;
+	char verify[26];
 	int i;
 	int nullcount=0;
 	printf("Enter bank code: ");
@@ -43,7 +45,6 @@ int main()
 	nullcount=nullcount+1;
 	}
 	printf("SK");
-	
 	printf("  ");
 	
 	if(bankcode>=100 and bankcode<=999)
@@ -55,10 +56,41 @@ int main()
 	{
 	printf("%d",bankcode);
 	}
+	
+	for(i=0;i<=3;i++)
+	{
+	int x=0;
+	if(bankcode>=100 and bankcode<=999)
+		{
+		verify[0]=0;
+		}
+	else
+		{
+		x=bankcode%10;
+		verify[i]=x;
+		bankcode=(bankcode/10);
+		}
+	}
+	
+	for(i=4;i<=9;i++)
+	{
+	verify[i]=rand()%1;
+	}
+	
 	printf("000000");
+	
 	for(i=1;i<=nullcount;i++)
 	{
 	printf("0");
 	}
 	printf("%lld",number);
+	
+	for(i=10;i<=19;i++)
+	{
+	int x=0;
+	x=number%10;
+	verify[i]=x;
+	number=(number/10);
+	}
+	//printf("\n\nPole: %s",verify);
 }
